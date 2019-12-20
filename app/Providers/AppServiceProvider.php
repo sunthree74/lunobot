a3e73984140c42481ac606505ec80bc49aead22e
Nur Muhlis <nurmuhlis74@yahoo.co.id>
2019-12-20 16:55:21 +0700

    url force schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('FORCE_HTTPS',false)) { // Default value should be false for local server
-            URL::forceSchema('https');
-        }
+        //
     }
 
     /**
